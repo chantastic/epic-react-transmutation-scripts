8381cfdeffaf3852e960eee43d572bfd80e460ab
Michael Chan <mijoch@gmail.com>
2023-10-05 17:40:28 -0700

    add feature to split exercise markdown into steps

--- modules/migrate_exercise.ts
+++ modules/migrate_exercise.ts
@@ -1,6 +1,8 @@
 import * as fs from "node:fs"
 import path from "node:path"
 import partial from "lodash/fp/partial"
+import kebabCase from "lodash/kebabCase"
+import {titleCase} from "title-case"
 
 export default function migrate_exercise({
 	id,
@@ -17,9 +19,26 @@ export default function migrate_exercise({
 	let shared_path = path.join(import.meta.url, "../..", project_path)
 	let new_exercises_path = path.join(shared_path, "exercises")
 
+	let exercise_title = "UNTITLED"
+
+	try {
+		exercise_title = fs
+			.readFileSync(
+				new URL(get_legacy_src_path("exercise", `${exercise_id}.md`)).pathname,
+				{
+					encoding: "utf-8",
+				}
+			)
+			.split(/^(?=#+ )/m)?.[0]
+			.replace(/# /, "")
+			.trim()
+	} catch (error) {
+		console.error(`file doesn't exsist: ${error.path}`)
+	}
+
 	let new_exercise_path = path.join(
 		new_exercises_path,
-		`${exercise_id}.UNTITLED`
+		`${exercise_id}.${kebabCase(exercise_title)}`
 	)
 
 	function get_new_exercise_path(...segments: string[]) {
@@ -143,7 +162,9 @@ export default function migrate_exercise({
 								`${String(offset_problem_id).padStart(
 									2,
 									"0"
-								)}.problem/index.html`
+								)}.problem/index.${get_suffix_from_file_name(
+									step_solution_file
+								)}`
 							)
 						).pathname
 					)
@@ -155,7 +176,157 @@ export default function migrate_exercise({
 			return acc
 		},
 		[])
-		.map(console.log)
+
+	try {
+		fs.readFileSync(new URL(get_new_exercise_path("README.mdx")).pathname, {
+			encoding: "utf-8",
+		})
+			.split(/^(?=#+ )/m)
+			.filter(Boolean)
+			.reduce(function (acc: string[], section) {
+				let exercise_section = /#.+Exercise/i
+
+				if (exercise_section.test(section)) {
+					let section_body = section
+						.split("\n")
+						.reduce(
+							(acc, line) => {
+								if (
+									line.startsWith("## ") ||
+									line.startsWith("Production deploy") ||
+									line.startsWith("- [Exercise") ||
+									line.startsWith("- [Final")
+								) {
+									return acc
+								}
+								return [...acc, line]
+							},
+							[`# ${titleCase(exercise_title)}`]
+						)
+						.join("\n")
+						.replace(/\n\n\n/, "\n")
+
+					try {
+						fs.writeFileSync(
+							new URL(get_new_exercise_path(`01.problem/README.mdx`)).pathname,
+							section_body,
+							{encoding: "utf-8"}
+						)
+					} catch (err) {
+						console.error(err)
+					}
+
+					try {
+						fs.writeFileSync(
+							new URL(get_new_exercise_path(`01.solution/README.mdx`)).pathname,
+							`# ${titleCase(exercise_title)}\n`,
+							{encoding: "utf-8"}
+						)
+					} catch (err) {
+						console.error(err)
+					}
+
+					return acc
+				}
+
+				return [...acc, section]
+			}, [])
+			.reduce(function (acc: string[], section, _, sections) {
+				if (section.match(/#.+\d/i)) {
+					let [legacy_extra_credit_id, legacy_title] =
+						section
+							.match(/#.+(\d).+\p{Emoji_Presentation}.(.+)\n/mu)
+							?.slice(1, 3) || []
+
+					let section_body = section
+						.split("\n")
+						.reduce(
+							(acc: string[], line) => {
+								if (
+									line.startsWith("### ") ||
+									line.startsWith("[Production deploy")
+								) {
+									return acc
+								}
+								return [...acc, line]
+							},
+							[`# ${titleCase(legacy_title)}`]
+						)
+						.join("\n")
+						.replace(/\n\n\n/, "\n\n")
+
+					if (Number(legacy_extra_credit_id) <= sections.length) {
+						try {
+							fs.writeFileSync(
+								new URL(
+									get_new_exercise_path(
+										`0${Number(legacy_extra_credit_id) + 1}.problem/README.mdx`
+									)
+								).pathname,
+								section_body,
+								{encoding: "utf-8"}
+							)
+						} catch (err) {
+							console.error(err)
+						}
+
+						try {
+							fs.writeFileSync(
+								new URL(
+									get_new_exercise_path(
+										`0${Number(legacy_extra_credit_id) + 1}.solution/README.mdx`
+									)
+								).pathname,
+								`# ${titleCase(legacy_title)}\n`,
+								{encoding: "utf-8"}
+							)
+						} catch (err) {
+							console.error(err)
+						}
+					}
+					return acc
+				}
+
+				return [...acc, section]
+			}, [])
+			.reduce(function (acc: string[], section) {
+				let exercise_section = /#.+Background/i
+
+				if (exercise_section.test(section)) {
+					let section_body = section
+						.split("\n")
+						.reduce(
+							(acc, line) => {
+								if (line.startsWith("## ")) {
+									return acc
+								}
+								return [...acc, line]
+							},
+							[`# ${titleCase(exercise_title)}`]
+						)
+						.join("\n")
+						.replace(/\n\n\n/, "\n")
+
+					console.log(section_body)
+
+					try {
+						fs.writeFileSync(
+							new URL(get_new_exercise_path(`README.mdx`)).pathname,
+							section_body,
+							{encoding: "utf-8"}
+						)
+					} catch (err) {
+						console.error(err)
+					}
+
+					return acc
+				}
+
+				return [...acc, section]
+			}, [])
+	} catch (error) {
+		console.error(`file doesn't exsist: ${error.path}`)
+	}
 }
 
 function get_suffix_from_file_name(file_name: string) {
